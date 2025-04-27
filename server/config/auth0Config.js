6d7f107ec667d868889a46e4cac125e4b3ea1611
Boogie Man <boogie@boogie.8.8.8.8>
2025-04-27 05:40:43 -0400

    [chore]: working changes

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,9 +1,10 @@
-import {auth} from 'express-oauth2-jwt-bearer'
-
+import { auth } from 'express-oauth2-jwt-bearer'
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
-    issuerBaseURL: "https://dev-03ifqltxbr6nn0hn.us.auth0.com",
+    audience: "http://localhost:8000" ,
+    issuerBaseURL: "https://dev-p78asudtfd1dqvzv.us.auth0.com",
     tokenSigningAlg: "RS256"
+    
+    
 })
 
 export default jwtCheck
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan'
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
+app.use(morgan('dev'))
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
